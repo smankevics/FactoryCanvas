76d3e35540a6ef309a9570053e8f159409e43da0
Nevidimko1 <s.mankevics@gmail.com>
2017-01-19 21:11:06 +0200

    webpack fixed paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
 	context: __dirname + '/app',
@@ -10,6 +11,10 @@ module.exports = {
 		path: __dirname + '/js',
 		filename: 'app.bundle.js'
 	},
+	resolve: {
+    root: path.resolve('./app'),
+    extensions: ['', '.js']
+  },
 	plugins: [
 		new webpack.SourceMapDevToolPlugin({
 			filename: '[file].map',
